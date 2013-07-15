520d80802468f7fc31e2d78f991e989c3084e407
tkanta <tarininegi@rediffmail.com>
2013-07-15 11:06:49 -0700

    ipn sample update

--- adaptiveaccountssample/src/main/java/com/sample/ipn/IPNListenerServlet.java
+++ adaptiveaccountssample/src/main/java/com/sample/ipn/IPNListenerServlet.java
@@ -22,7 +22,7 @@ public class IPNListenerServlet extends HttpServlet {
 		// Configuration map containing signature credentials and other required configuration.
 		// For a full list of configuration parameters refer at 
 		// (https://github.com/paypal/adaptiveaccounts-sdk-java/wiki/SDK-Configuration-Parameters)
-		Map<String,String> configurationMap =  Configuration.getSignatureConfig();
+		Map<String,String> configurationMap =  Configuration.getIpnConfig();
 		IPNMessage ipnlistener = new IPNMessage(request,configurationMap);
 		boolean isIpnVerified = ipnlistener.validate();
 		Map<String,String> map = ipnlistener.getIpnMap();
