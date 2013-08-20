a58c99a69ec83b6554c296bef3cc0de8ab01e379
Latha <lathavairamani@gmail.com>
2013-08-21 01:45:10 +0530

    Update Configuration.java

--- adaptiveaccountssample/src/main/java/com/sample/adaptiveaccounts/Configuration.java
+++ adaptiveaccountssample/src/main/java/com/sample/adaptiveaccounts/Configuration.java
@@ -18,7 +18,7 @@ public class Configuration {
 		configMap.put("acct1.UserName", "jb-us-seller_api1.paypal.com");
 		configMap.put("acct1.Password", "WX4WTU3S8MY44S7F");
 		configMap.put("acct1.Signature", "AFcWxV21C7fd0v3bYYYRCpSSRl31A7yDhhsPUU2XhtMoZXsWHFxu-RWy");
-	    configMap.put("acct1.AppId", "APP-80W284485P519543T");
+	    	configMap.put("acct1.AppId", "APP-80W284485P519543T");
 		
 		// Sample Certificate credential
 		// configMap.put("acct2.UserName", "certuser_biz_api1.paypal.com");
@@ -26,6 +26,9 @@ public class Configuration {
 		// configMap.put("acct2.CertKey", "password");
 		// configMap.put("acct2.CertPath", "resource/sdk-cert.p12");
 		// configMap.put("acct2.AppId", "APP-80W284485P519543T");
+		
+		// Sandbox Email Address
+		configMap.put("service.sandboxEmailAddress", "pp.devtools@gmail.com");
 		
 		return configMap;
 	}
